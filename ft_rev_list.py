57cedb1818c23ec4d2ee767818db71c51ecd7c2b
катя <45289493+rfnzrfnzrfnz@users.noreply.github.com>
2020-11-05 10:01:32 +0300

    Update ft_rev_list.py

--- ft_rev_list.py
+++ ft_rev_list.py
@@ -1,2 +1,14 @@
+def ft_len(mass):
+    l = 0
+    for i in mass:
+        l += 1
+    return (l)
+
+
 def ft_rev_list(mass):
-    return mass[::-1]
+    c = ft_len(mass)
+    for i in range(0, c // 2):
+        a = mass[i]
+        mass[i] = mass[-i - 1]
+        mass[-i - 1] = a
+    return mass
